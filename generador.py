6ad1cb691f9c5d706279abb2b37dc4372a8fab9e
anonimo <anonimo@anonimo.com>
2017-05-18 21:26:55 -0500

    add generator

--- generador.py
+++ generador.py
@@ -1,11 +1,25 @@
+
 class Generador:
+    def generadorTituloParrafo(self, tirulo, parrafo):
+        titulo = "<h1 >" + titulo + "</h1>"
+        parrafo = "<p>" + parrado + "</p>"
+        return tirulo + parrafo
 
-    def generadorTituloParrafo(self, titulo, parrafo):
-        titulo = "<h1>" + titulo + "</h1>"
-        parrafo = "<p>" + parrafo + "</p>"
-        return titulo + parrafo
+    def generadorLista(self,Lista):
+        codigo =""
+        for i in Lista:
+            codigo = codigo + "<li >" + i + "</li>"
+        return "<ol>" + codigo + "</ol>"
 
-    def nombre(self, file):
-        for k in file:
-            for l in k.split(","):
-                print (l)
+    def generadorTabla(self, listaTabla):
+        tabla = ""
+        td = ""
+        for i in listaTabla:
+            tr1 = "<tr>"
+            for j in i.split(","):
+                td = td + "<td >" + j + "</td>"
+            tr2 = "</tr>"
+
+            tabla = tabla + tr1 + td + tr2
+            td =""
+        return "<table>" + tabla + "</table>"
